c6b44a88377ca787870f6ad5570d5c479e12b027
YoussefIbraheem <youssefm.muhammed@gmail.com>
2025-04-26 21:20:49 +0300

    Add home page redirect to list books; include necessary imports and templates

--- book_store_fast_api/main.py
+++ book_store_fast_api/main.py
@@ -1,15 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Request
+from fastapi.responses import RedirectResponse
 from .core.database import Base , engine
 from .routes.book import router as book_router
 from .routes.author import router as author_router
 from .views.book import router as book_web_routes
+from fastapi.templating import Jinja2Templates
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
-
+templates = Jinja2Templates(directory="templates")
 app.include_router(book_router)
 app.include_router(author_router)
 app.include_router(book_web_routes)
+@app.route('/')
+def home_page(request:Request):
+   return RedirectResponse(
+        request.url_for("web_list_books"), status_code=303
+    )
 
 if __name__ == "__main__":
     import uvicorn
