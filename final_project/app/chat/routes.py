0135df625abc6188af551ffbeee2676b22f95962
YoussefIbraheem <youssefm.muhammed@gmail.com>
2025-06-20 22:45:25 +0300

    Refactor chat functionality to improve message handling and add loading spinner during message processing

--- final_project/app/chat/routes.py
+++ final_project/app/chat/routes.py
@@ -6,16 +6,17 @@ from app.chat import bp
 from app.chat.ai import initialize_sql_agent
 
 
-
+@login_required
 @bp.route('/chat', methods=['GET', 'POST'])
 def chat():
     return render_template('chat/chat.html')
 
 
+@login_required
 @socketio.on('sending-message')
 def handle_send_message(data):
     message = data['message']
     print(f"Received message: {message}")
     agent = initialize_sql_agent()
     response = agent.invoke({"input": message})
-    print(f"Agent response: {response['output']}")
+    socketio.emit('receive-message', {'message': response['output']})
